c5e0ab9b7160fbb5b37a8658fa3a4d3985f75e1f
kalboussi karim <kalboussikarim3@gmail.com>
2025-01-03 12:10:45 +0100

    passed browser parameter from the XML file

--- src/test/java/ParamTest.java
+++ src/test/java/ParamTest.java
@@ -1,18 +1,31 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.util.Objects;
 
 public class ParamTest {
 
     WebDriver driver;
 
     @BeforeClass
-    void setup() throws InterruptedException {
-        driver = new ChromeDriver();
+    @Parameters({"browser"})
+    void setup(String browser) throws InterruptedException {
+        switch (browser){
+            case "chrome" :  driver = new ChromeDriver(); break ;
+            case "edge" :    driver = new EdgeDriver(); break ;
+            case "firefox" : driver = new FirefoxDriver(); break ;
+            default:
+                System.out.println("Invalid browser.");
+                return ;
+        }
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().window().maximize();
         Thread.sleep(2000);
